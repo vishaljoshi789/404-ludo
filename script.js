62bf1ca6af6e475303b372958ca152a852edfd83
Vishal <vishalwebdev0987@gmail.com>
2022-10-16 01:13:07 +0530

    logic added

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let verticalArea1 = document.querySelector(".vertical-path-1");
 let verticalArea2 = document.querySelector(".vertical-path-2");
 let horizontalArea1 = document.querySelector(".horizontal-path-1");
 let horizontalArea2 = document.querySelector(".horizontal-path-2");
-
+let dice = document.querySelector("#dice-roll-btn");
 
 
 
@@ -29,7 +29,7 @@ let coinPath = ["05", "08", "11", "14", "17", "315", "312", "39", "36", "33", "3
 
 for (let j = 0; j < 4; j++) {
     for (let i = 0; i < 18; i++) {
-        let pathClasses = ["w-1/3", "h-1/6", "border-black", "border-y", "border-x"]
+        let pathClasses = ["w-1/3", "h-1/6", "border-black", "border-y", "border-x", "flex", "items-center", "justify-center", "pathCell"]
         let pathbox = document.createElement("div");
         pathClasses.forEach((e) => {
             pathbox.classList.add(e);
@@ -74,23 +74,76 @@ let greenVictoryPath = ["213", "210", "27", "24", "21"];
 
 let safeBoxes = ["06", "111", "36", "211"]
 
-blueVictoryPath.forEach((e)=>{
+blueVictoryPath.forEach((e) => {
     document.querySelector(`#path-box-${e}`).classList.add("bg-blue-400");
 })
-yellowVictoryPath.forEach((e)=>{
+yellowVictoryPath.forEach((e) => {
     document.querySelector(`#path-box-${e}`).classList.add("bg-yellow-400");
 })
-redVictoryPath.forEach((e)=>{
+redVictoryPath.forEach((e) => {
     document.querySelector(`#path-box-${e}`).classList.add("bg-red-400");
 })
-greenVictoryPath.forEach((e)=>{
+greenVictoryPath.forEach((e) => {
     document.querySelector(`#path-box-${e}`).classList.add("bg-green-400");
 })
 
 
-safeBoxes.forEach((e)=>{
+safeBoxes.forEach((e) => {
     document.querySelector(`#path-box-${e}`).classList.add("bg-gray-300");
 })
+
+dice.addEventListener("click", () => {
+    document.querySelector(`#dice-number`).innerText = Math.floor(Math.random() * 6) + 1
+})
+
+
+
+let coinReplacer = (e, initial) => {
+        console.log("clicked")
+            if (e.parentNode.classList.contains("coin-container")) {
+                document.querySelector(`#path-box-${initial}`).appendChild(e.cloneNode(true))
+                e.parentNode.removeChild(e)
+
+            } else if (e.parentNode.classList.contains("pathCell")) {
+                console.log(e.parentNode.id)
+            }
+
+            coinEventAdder()
+        
+    
+}
+
+let coinEventAdder = () => {
+    document.querySelectorAll(".coin").forEach((e) => {
+
+        e.addEventListener("click", () => {
+        // console.log("clicked")
+
+            if (e.classList.contains("greencoin")) {
+                
+                let greenInitial = "212"; coinReplacer(e, greenInitial)
+            }
+            else if (e.classList.contains("bluecoin")) {
+                let blueInitial = "05"; coinReplacer(e, blueInitial)
+            }
+            else if (e.classList.contains("redcoin")) {
+
+                let redInitial = "112"; coinReplacer(e, redInitial)
+            }
+            else if (e.classList.contains("yellowcoin")) {
+                let yellowInitial = "35"; coinReplacer(e, yellowInitial)
+            }
+        })
+    })
+};coinEventAdder()
+
+
+
+
+
+
+
+
 
 
 // middleArea.style.height = "34%";
